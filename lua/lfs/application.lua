597b6b3f8a3575c7d7d775c7308cb686b32e1f0c
Firenox89 <firenox89@gmail.com>
2020-04-22 01:21:03 +0200

    add controls.html

--- lua/lfs/application.lua
+++ lua/lfs/application.lua
@@ -1,9 +1,3 @@
-function startLEDs()
-    print("start LEDS")
-    ws2812.init()
-    ws2812.write(string.char(255, 0, 0, 0, 255, 0, 0, 0))
-end
-
 function setupWebServer()
     print("setup webserver")
     dofile('httpServer.lua')
@@ -26,17 +20,32 @@ function setupWebServer()
 
     ws2812.init()
 
-    httpServer:use('/test', function(req, res)
-        print("test")
-        res:send('test')
-    end)
     httpServer:use('/off', function(req, res)
-        buffer = ws2812.newBuffer(200, 4)
-        buffer:fill(0,0,0,0)
-        ws2812.write(buffer)
-        res:send('Off')
+        level = tonumber(req.query.level)
+        print("level " .. level)
+        if level == 0 then
+            completeLEDBuffer:fill(0,0,0,0)
+        elseif level == 1 then
+            levelBuffer = ledLevelBuffer[level]
+            levelBuffer:fill(0, 0, 0, 0)
+            completeLEDBuffer:replace(ledLevelBuffer[level],ledCountPerLevel*level-1)
+        elseif level == 2 then
+            levelBuffer = ledLevelBuffer[level]
+            levelBuffer:fill(0, 0, 0, 0)
+            completeLEDBuffer:replace(ledLevelBuffer[level],ledCountPerLevel*level-1)
+        elseif level == 3 then
+            levelBuffer = ledLevelBuffer[level]
+            levelBuffer:fill(0, 0, 0, 0)
+            completeLEDBuffer:replace(ledLevelBuffer[level],ledCountPerLevel*level-1)
+        elseif level == 4 then
+            levelBuffer = ledLevelBuffer[level]
+            levelBuffer:fill(0, 0, 0, 0)
+            completeLEDBuffer:replace(ledLevelBuffer[level],ledCountPerLevel*level-1)
+        end
+        ws2812.write(completeLEDBuffer)
+        res:sendFile('controls.html')
     end)
-    httpServer:use('/fill', function(req, res)
+    httpServer:use('/update', function(req, res)
         level = tonumber(req.query.level)
         r = tonumber(req.query.r)
         g = tonumber(req.query.g)
@@ -52,44 +61,12 @@ function setupWebServer()
             levelBuffer:fill(r, g, b, w)
             completeLEDBuffer:replace(ledLevelBuffer[level+1],ledCountPerLevel*level+1)
             ws2812.write(completeLEDBuffer)
-            res:send("ok")
         end
+        res:sendFile('controls.html')
     end)
-    httpServer:use('/white', function(req, res)
-        completeLEDBuffer:fill(255,255,255,255)
-        ws2812.write(completeLEDBuffer)
-        res:send(completeLEDBuffer:dump())
-    end)
-    httpServer:use('/red', function(req, res)
-        completeLEDBuffer:fill(0,255,0,111)
-        ws2812.write(completeLEDBuffer)
-        res:send(completeLEDBuffer:dump())
-    end)
-    httpServer:use('/blau1', function(req, res)
-        ledLevelBuffer[1]:fill(0,255,0,0)
-        completeLEDBuffer:replace(ledLevelBuffer[1],ledCountPerLevel)
-        ws2812.write(completeLEDBuffer)
-        res:send('red')
+    httpServer:use('/', function(req, res)
+        res:sendFile('controls.html')
     end)
 end
-
--- a simple telnet server
-s=net.createServer(net.TCP)
-s:listen(2323,function(c)
-   con_std = c
-   function s_output(str)
-      if(con_std~=nil)
-         then con_std:send(str)
-      end
-   end
-   node.output(s_output, 0)   -- re-direct output to function s_ouput.
-   c:on("receive",function(c,l)
-      node.input(l)           -- works like pcall(loadstring(l)) but support multiple separate line
-   end)
-   c:on("disconnection",function(c)
-      con_std = nil
-      node.output(nil)        -- un-regist the redirect output function, output goes to serial
-   end)
-end)
 
 setupWebServer()
--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-~/apps/nodemcu-firmware-3.0-master_20190907/luac.cross -f -o lfs.img lua/lfs/* && curl 192.168.2.111/OTA
+~/apps/nodemcu-firmware-3.0-master_20190907/luac.cross -f -o lfs.img lua/lfs/* && curl 192.168.2.101/OTA
